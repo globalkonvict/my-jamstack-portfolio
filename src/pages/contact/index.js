af5c2df4b86dc8d1a641403214ab661c2d4711a9
Sarthak Dwivedi <sarthakwarlock@gmail.com>
2022-05-24 15:52:02 +0530

    - added contact form and thank you screen

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -1,93 +1,54 @@
 import * as React from 'react';
-import { navigate } from 'gatsby';
-import sendEMail from '@src/services/contact';
+import { useLocation } from 'react-use';
 
-function encode(data) {
-  return Object.keys(data)
-    .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
-    .join('&');
-}
+const ContactPage = () => {
+  const { origin } = useLocation();
 
-export default class Index extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { isValidated: false };
-  }
-
-  handleChange = e => {
-    this.setState({ [e.target.name]: e.target.value });
-  };
-
-  handleSubmit = async e => {
-    e.preventDefault();
-    try {
-      const { from, message, email } = e.target;
-      const send = await sendEMail({ from: from.value, message: message.value, email: email.value });
-      console.log(send);
-      if (send) {
-        navigate('/contact/success');
-      }
-    } catch (err) {
-      if (err) {
-        return console.error(err);
-      }
-    }
-  };
-
-  render() {
-    return (
-      <section className='section'>
-        <div className='container'>
-          <div className='content'>
-            <h1>Contact</h1>
-            <form
-              name='contact'
-              method='post'
-              action='/contact/thanks/'
-              data-netlify='true'
-              data-netlify-honeypot='bot-field'
-              onSubmit={this.handleSubmit}
-            >
-              {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
-              <input type='hidden' name='form-name' value='contact' />
-              <div hidden>
-                <label>
-                  Don’t fill this out: <input name='bot-field' onChange={this.handleChange} />
-                </label>
+  return (
+    <section className='section'>
+      <div className='container'>
+        <div className='content'>
+          <h1>Contact</h1>
+          <form name='contact' action='https://formsubmit.co/sarthakwarlock@gmail.com' method='POST'>
+            {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+            <input type='hidden' name='form-name' value='contact' />
+            <input type='hidden' name='_next' value={`${origin}/contact/thanks`} />
+            <input type='hidden' name='_captcha' value='false' />
+            <input type='hidden' name='_subject' value='Message from Global Konvict'></input>
+            <div className='field'>
+              <label className='label' htmlFor={'name'}>
+                Your name
+              </label>
+              <div className='control'>
+                <input className='input' type={'text'} name={'name'} id={'name'} required={true} />
               </div>
-              <div className='field'>
-                <label className='label' htmlFor={'from'}>
-                  Your name
-                </label>
-                <div className='control'>
-                  <input className='input' type={'text'} name={'from'} onChange={this.handleChange} id={'name'} required={true} />
-                </div>
+            </div>
+            <div className='field'>
+              <label className='label' htmlFor={'email'}>
+                Email
+              </label>
+              <div className='control'>
+                <input className='input' type={'email'} name={'email'} id={'email'} required={true} />
               </div>
-              <div className='field'>
-                <label className='label' htmlFor={'email'}>
-                  Email
-                </label>
-                <div className='control'>
-                  <input className='input' type={'email'} name={'email'} onChange={this.handleChange} id={'email'} required={true} />
-                </div>
+            </div>
+            <div className='field'>
+              <label className='label' htmlFor={'message'}>
+                Message
+              </label>
+              <div className='control'>
+                <textarea className='textarea' name={'message'} id={'message'} required={true} />
               </div>
-              <div className='field'>
-                <label className='label' htmlFor={'message'}>
-                  Message
-                </label>
-                <div className='control'>
-                  <textarea className='textarea' name={'message'} onChange={this.handleChange} id={'message'} required={true} />
-                </div>
-              </div>
-              <div className='field'>
-                <button className='button is-link' type='submit'>
-                  Send
-                </button>
-              </div>
-            </form>
-          </div>
+            </div>
+            <div className='field'>
+              <button className='button is-link' type='submit'>
+                Send
+              </button>
+            </div>
+          </form>
         </div>
-      </section>
-    );
-  }
-}
+      </div>
+    </section>
+  );
+};
+
+export default ContactPage;
--- src/pages/contact/thanks.js
+++ src/pages/contact/thanks.js
@@ -1,9 +1,15 @@
+import Lottie from 'react-lottie-player';
+import ThankYouAnimated from '@src/img/lottie/thank-you.json';
+
 const ThankYou = () => (
   <section className='section'>
     <div className='container'>
       <div className='content'>
-        <h1>Thank you!</h1>
-        <p>This is a custom thank you page for form submissions</p>
+        <div className='has-text-centered'>
+          <h1 className='is-size-2'>Thank you!</h1>
+          <p className='is-size-4'>Message sent successfully</p>
+        </div>
+        <Lottie loop animationData={ThankYouAnimated} play style={{ width: '100%', height: '100%' }} />
       </div>
     </div>
   </section>
